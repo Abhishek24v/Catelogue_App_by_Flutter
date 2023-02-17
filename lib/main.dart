6031c9363304545b71d7c67af9a813123c3bbf6f
Abhishek Vishwakarma <abhishek.vishwakarma@betaflux.co>
2023-02-17 17:25:24 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_catelog_app/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(
-          child: Text("Welcome to Flutter by Abhishek"),
-        ),
-      ),
-    ));
+    return const MaterialApp(
+      home: HomePage(),
+    );
   }
 }
